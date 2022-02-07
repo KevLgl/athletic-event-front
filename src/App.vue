fd4fe093331a0a91e937a401c84ba5768e931c32
Kevin Laguel <laguel.kev@gmail.com>
2022-02-07 17:00:54 +0100

    feat: ajout de tailwindCss

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,11 @@
   <Events />
   <h1>Les categorys: </h1>
    <Categorys />
+     <div class="justify-center flex bg-yellow-300 items-center h-screen">
+    <div class="text-4xl">
+      Hello 👋🏼
+    </div>
+  </div>
 </template>
 
 <script>
